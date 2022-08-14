93943f737adec938170d5009ea45d054a3ebe024
Jesper Kuutti <jesper.kuutti@gmail.com>
2022-08-14 16:07:39 +0300

    Rough layout, popup menu
    Also added sprites, did some groundwork with TypeScript to be able to fetch html elements with correct types.

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,49 @@
+import { ElementsByTagNames, TagName, isElementWithTag } from "./utils/type_checkers";
 
-function printMessage(message: string) {
-    console.log(message)
+
+/**
+ * Find references to all the relevant components
+ */
+const gameMenuBtn = getElementWithId('game-menu-btn', 'BUTTON')
+const gameMenuDiv = getElementWithId('game-menu', 'DIV')
+
+const gameMenuHandler = {
+    init: () => {
+        gameMenuHandler.menuBtn.onclick = gameMenuHandler.toggleVisibility
+    },
+    menuBtn: gameMenuBtn,
+    menu: gameMenuDiv,
+    visible: false,
+    toggleVisibility: () => {
+        console.log("Toggling!")
+        if (gameMenuHandler.menu.className.includes('invisible')) {
+            gameMenuHandler.menu.className = gameMenuHandler.menu.className.replace('invisible', 'visible')
+        } else {
+            gameMenuHandler.menu.className = gameMenuHandler.menu.className.replace('visible', 'invisible')
+        }
+    }
 }
 
-printMessage("Hello Worldded!")
+
+function getElementWithId<T extends TagName>(id: string, tagName: T): ElementsByTagNames[T] {
+    const el = document.getElementById(id)  
+    if (el !== null && isElementWithTag(el, tagName)) {
+        return el
+    } else {
+        console.error(`Element with id ${id} is not of type ${tagName}`)
+    }
+
+    console.error(`Was not able to find element with id ${id}. The type checks might be at fault.`)
+    throw new Error("Cannot continue without the element.")
+}
+
+
+function attachEventListeners() {
+    gameMenuHandler.init()
+}
+
+function main() {
+    attachEventListeners();
+}
+
+main()
---
+++ ts/utils/type_checkers.ts
@@ -0,0 +1,18 @@
+
+export type ElementsByTagNames = {
+    'BUTTON': HTMLButtonElement
+    'DIV': HTMLDivElement,
+    'P': HTMLParagraphElement
+}
+
+export type TagName = keyof ElementsByTagNames 
+
+
+export function isElementWithTag<T extends TagName>(el: HTMLElement | null, name: T): el is ElementsByTagNames[T]{
+    if (el === null) {
+        return false
+    }
+
+    // This should work, as long as the tag names are not changed (and I copy them correctly)... But I doubt that will happen
+    return el.tagName === name
+}
