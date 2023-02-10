7487c79693c06283a25b5e5aa35274fdef93b707
Jesper Kuutti <jesper.kuutti@gmail.com>
2023-02-10 15:39:55 +0200

    Added log

--- ts/ios_button_fix.ts
+++ ts/ios_button_fix.ts
@@ -1,5 +1,6 @@
 export default function fixButtonsOniOS() {
-    if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
-        document.body.addEventListener('touchstart', function() {}, false);
-    }
+  if (/iP(hone|ad)/.test(window.navigator.userAgent)) {
+    document.body.addEventListener('touchstart', function () {}, false);
+    console.log('fixeed!');
+  }
 }
